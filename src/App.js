4e457242d2816391f56f5eb3f152a8d8987ea8fc
doyun0327 <idoyun781@gmail.com>
2026-03-29 15:31:56 +0900

    git push test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // 백엔드에 로그인 세션 확인 요청!!!!!
+    // 백엔드에 로그인 세션 확인 요청!!!!
     axios
       .get("http://localhost:8080/api/user/me", { withCredentials: true })
       .then((res) => {
@@ -27,35 +27,35 @@ function App() {
     );
 
   return (
-  <div>
-    {user ? (
-      <>
-        <Header user={user} />
-        <div style={{ padding: "40px", textAlign: "center" }}>
-          <h2>🎉 환영합니다!</h2>
+    <div>
+      {user ? (
+        <>
+          <Header user={user} />
+          <div style={{ padding: "40px", textAlign: "center" }}>
+            <h2>🎉 환영합니다!</h2>
 
-          <p style={{ fontSize: "18px", color: "#555" }}>
-            {/* 1. 카카오 로그인인 경우 (properties.nickname 존재) */}
-            {user.properties?.nickname ? (
-              <>
-                <strong>{user.properties.nickname}</strong>님 (카카오 계정)
-              </>
-            ) : (
-              /* 2. 일반 이메일 로그인인 경우 */
-              <>
-                <strong>{user.email || user.username}</strong>님 (이메일 계정)
-              </>
-            )}
-            으로 로그인되었습니다.
-          </p>
+            <p style={{ fontSize: "18px", color: "#555" }}>
+              {/* 1. 카카오 로그인인 경우 (properties.nickname 존재) */}
+              {user.properties?.nickname ? (
+                <>
+                  <strong>{user.properties.nickname}</strong>님 (카카오 계정)
+                </>
+              ) : (
+                /* 2. 일반 이메일 로그인인 경우 */
+                <>
+                  <strong>{user.email || user.username}</strong>님 (이메일 계정)
+                </>
+              )}
+              으로 로그인되었습니다.
+            </p>
 
-          <p>이제 ITDA 서비스를 마음껏 이용해 보세요. 🐱✨</p>
-        </div>
-      </>
-    ) : (
-      <SignupWithSocial />
-    )}
-  </div>
+            <p>이제 ITDA 서비스를 마음껏 이용해 보세요. 🐱✨</p>
+          </div>
+        </>
+      ) : (
+        <SignupWithSocial />
+      )}
+    </div>
   );
 }
 
